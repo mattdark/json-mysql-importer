64dc9e781bdfc32d98f8436e840a6d1c6ed4d3d5
Mario García <iscmariog@gmail.com>
2022-09-08 10:02:11 -0500

    load_data function updated

--- read_load_data.py
+++ read_load_data.py
@@ -1,19 +1,30 @@
-from modules import countries
-from modules import genres
-from modules import schema
+from modules.countries import Country_Dict
+from modules.genres import Genre_Dict
+
+from modules.schema import Country, Genre, Movie, Director, Actor, Writer
+from modules.base import Session, engine, Base
+
 import pandas as pd
 from tqdm import tqdm
 import os
 import json
 from glob import glob
 
+# 2 - generate database schema
+Base.metadata.create_all(engine)
+
+# 3 - create a new session
+session = Session()
+
 def create_dataframe(filepath):
     json_pattern = os.path.join(filepath, '*.json')
     file_list = glob(json_pattern)
 
     json_list = []
-    for file in tqdm(file_list, desc='Creating DataFrame'):
-        with open (file) as f:
+    #for file in tqdm(file_list, desc='Creating DataFrame'):
+    for i in tqdm(range(499), desc='Creating DataFrame'):
+        #with open (file) as f:
+        with open(file_list[i]) as f:
             exp = json.load(f)
             json_list.append(exp)
 
@@ -21,53 +32,61 @@ def create_dataframe(filepath):
     return df
 
 def create_catalog():
-    for key, value in countries.Country.items():
-        statement = schema.countries.insert().values(country_id=value, name=key)
-        schema.engine.execute(statement)
+    for key, value in Country_Dict.items():
+        country = Country(value, key)
+        session.add(country)
 
-    for key, value in genres.Genre.items():
-        statement = schema.genres.insert().values(genre_id=value, name=key)
-        schema.engine.execute(statement)
+    for key, value in Genre_Dict.items():
+        genre = Genre(value, key)
+        session.add(genre)
+    
+    session.commit()
 
 def load_data(df):
     for row in tqdm (df.itertuples(), desc='Loading Data'):
         country = None
-        if row.country in countries.Country:
-            country = countries.Country[row.country]
-
-        stm = schema.movies.insert().values(movie_id=row.imdb_id, title=row.title, country=country)
-        schema.engine.execute(stm)
+        if row.country in Country_Dict:
+            country = Country_Dict[row.country]
+        movie = Movie(row.imdb_id, row.title, country)
 
         if row.genres is not None:
             for val in row.genres:
-                if val in genres.Genre:
-                    genre = genres.Genre[val]
-                    stm = schema.movie_genres.insert().values(movie_id=row.imdb_id, genre_id=genre)
-                    schema.engine.execute(stm)
+                if val in Genre_Dict:
+                    genre = Genre(Genre_Dict[val], val)
+                    movie.genres.append(genre)
 
         if row.director != [None]:
             for data in row.director or []:
-                stm = schema.director.insert().values(director_id=data['id'], name=data['name']).prefix_with('IGNORE')
-                schema.engine.execute(stm)
-                stm = schema.movie_director.insert().values(movie_id=row.imdb_id, director_id=data['id'])
-                schema.engine.execute(stm)
+                director = Director(data['id'], data['name'])
+                session.merge(director)
+                session.flush()
+                movie.directors.append(director)
 
         if row.cast:
             for data in row.cast:
-                stm = schema.cast.insert().values(cast_id=data['id'], name=data['name']).prefix_with('IGNORE')
-                schema.engine.execute(stm)
-                stm = schema.movie_cast.insert().values(movie_id=row.imdb_id, cast_id=data['id'])
-                schema.engine.execute(stm)
+                actor = Actor(data['id'], data['name'])
+                session.merge(actor)
+                session.flush()
+                movie.actors.append(actor)
 
         if row.writer:
-            for data in row.writer:
-                stm = schema.writers.insert().values(writer_id=data['id'], name=data['name']).prefix_with('IGNORE')
-                schema.engine.execute(stm)
-                stm = schema.movie_writers.insert().values(movie_id=row.imdb_id, writer_id=data['id']).prefix_with('IGNORE')
-                schema.engine.execute(stm)
+            done = set()
+            w = []
+            for d in row.writer:
+                if d['id'] not in done:
+                    done.add(d['id'])
+                    w.append(d)
+            for data in w:
+                writer = Writer(data['id'], data['name'])
+                session.merge(writer)
+                session.flush()
+                movie.writers.append(writer)
+        
+        session.merge(movie)
+        session.flush()
+    session.commit()
 
 df = create_dataframe('./datasets/')
-chunks = np.array_split(df, 100)
-schema.meta.create_all(schema.engine)
 create_catalog()
 load_data(df)
+session.close()
